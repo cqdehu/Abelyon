badd7980e6fa4ea7ee217bca667e6023998c920a
Lehotaiabel <cqde.hu@gmail.com>
2023-06-23 13:52:11 +0200

    :D

--- exit.php
+++ exit.php
@@ -3,3 +3,6 @@
 setcookie('operation', '', time() - 3600, '/');
 setcookie('difficulty', '', time() - 3600, '/');
 setcookie('level', '', time() - 3600, '/');
+
+header('Location: https://www.abelyon.com/home.html');
+exit();
