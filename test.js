b7ef61cea010ef2494a5f4b2f4f9897b4baee608
Lehotaiabel <cqde.hu@gmail.com>
2023-06-23 23:40:26 +0200

    javitas

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function getCookie() {
     $.ajax({
         type: "POST",
         url: "getCookie.php",
-        data: { json_data: json },
+        data: { json_data: "json" },
         success: function (response) {
             var list = JSON.parse(response);
             console.log(list);
