42ff4dc50ac086787a74ca2ad0bcca2d395f2898
Lehotaiabel <cqde.hu@gmail.com>
2023-06-24 00:22:41 +0200

    :D

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ function getCookie() {
             var operation = list[0]
             var difficulty = list[1]
             var level = list[2]
-            console.log(operation)
+            $("#operation").text(operation)
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
