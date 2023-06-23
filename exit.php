de36b1a27ba377e8a71c55000eac04ede0182d93
Lehotaiabel <cqde.hu@gmail.com>
2023-06-23 13:53:36 +0200

    :D

--- exit.php
+++ exit.php
@@ -4,5 +4,5 @@ setcookie('operation', '', time() - 3600, '/');
 setcookie('difficulty', '', time() - 3600, '/');
 setcookie('level', '', time() - 3600, '/');
 
-header('Location: https://www.abelyon.com/home.html');
+header('Location: https://abelyon.com/home.html');
 exit();
