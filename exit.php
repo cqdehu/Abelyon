7ec42f420d61055d0b6e94cef1a7af29046a553c
Lehotaiabel <cqde.hu@gmail.com>
2023-06-28 13:42:22 +0200

    a

--- exit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-setcookie('operation', '', time() - 3600, '/');
-setcookie('difficulty', '', time() - 3600, '/');
-setcookie('level', '', time() - 3600, '/');
-
-echo "/home.html";
--- secure.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (isset($_COOKIE['operation']) && isset($_COOKIE['difficulty']) && isset($_COOKIE['level'])) {
-    // Mindhárom cookie létezik
-} else {
-    // Legalább egy cookie hiányzik
-    echo "/home.html";
-}
