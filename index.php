0761d226826e23cc409b5271384c63fddb54b76d
Lehotaiabel <cqde.hu@gmail.com>
2023-06-28 03:10:24 +0200

    php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 <body class="bg-dark">
 
-    <div class="flex flex-col w-full">
+    <div class="flex flex-col w-full justify-center">
         <div>1</div>
         <div>2</div>
         <div>3</div>
