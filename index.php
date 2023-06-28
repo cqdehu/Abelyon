745648dfe8e21146585362c32293d186271f5d79
Lehotaiabel <cqde.hu@gmail.com>
2023-06-28 03:27:40 +0200

    php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 <body class="bg-neutral-900">
 
-    <div class="flex justify-center h-full">
+    <div class="flex justify-center content-center">
         <form action="index.php" method="get">
             <label for="">Username</label>
             <input type="text" name="username">
