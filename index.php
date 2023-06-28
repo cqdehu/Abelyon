d762abfb23a3aaf70febdcb05c09c6f38eb4b585
Lehotaiabel <cqde.hu@gmail.com>
2023-06-28 02:48:40 +0200

    php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.0/dist/js.cookie.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+</head>
+
+<body class="bg-dark">
+
+    <main class="container">
+        <div class="row">
+            <div class="col">
+                <p class="p-0 m-0 text-light fw-bold" id="operation">Addition</p>
+            </div>
+            <div class="col">
+                <p class="p-0 m-0 text-light fw-bold" id="difficulty">Easy</p>
+            </div>
+            <div class="flex flex">
+                <p class="text-3xl font-bold" id="level">0</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p class="p-0 m-0 text-light fw-bold" id="operator">-</p>
+            </div>
+        </div>
+    </main>
+
+    <script src="/test.js"></script>
+</body>
+
+</html>
